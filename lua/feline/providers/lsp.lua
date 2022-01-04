f2423342a39602ac0efa83024a01b635c95e7128
kar9222 <karchua922@gmail.com>
2022-01-04 21:41:50 +1100

    CUSTOM: Fix LSP diagnostics
    - lsp.lua is copied directly from feline-nvim/feline.nvim (commit e54e0cc).
    - See `:h deprecated`. Change from `vim.lsp.diagnostic.get_count()` to vim.diagnostic.get().

--- lua/feline/providers/lsp.lua
+++ lua/feline/providers/lsp.lua
@@ -1,61 +1,63 @@
 local M = {}
 
+local lsp = vim.lsp
+local diagnostic = vim.diagnostic
+
+local severity_names = { 'Error', 'Warning', 'Information', 'Hint' }
+
 function M.is_lsp_attached()
-    return next(vim.lsp.buf_get_clients()) ~= nil
+    return next(lsp.buf_get_clients(0)) ~= nil
 end
 
 function M.get_diagnostics_count(severity)
-    local bufnr = vim.api.nvim_get_current_buf()
-    local active_clients = vim.lsp.buf_get_clients(bufnr)
-    
-    if not active_clients then return nil end
-
-    local count = 0
-    for _, client in pairs(active_clients) do
-        count = count + vim.lsp.diagnostic.get_count(bufnr, severity, client.id)
+    if vim.fn.has('nvim-0.6') == 1 then
+        return vim.tbl_count(diagnostic.get(0, severity and { severity = severity }))
+    else
+        -- TODO: drop this when 0.5 is no longer used
+        return lsp.diagnostic.get_count(0, severity and severity_names[severity])
     end
-
-    return count
 end
 
 function M.diagnostics_exist(severity)
-    local diagnostics_count = M.get_diagnostics_count(severity)
-    return diagnostics_count and diagnostics_count > 0
+    return M.get_diagnostics_count(severity) > 0
 end
 
-function M.lsp_client_names(component)
+function M.lsp_client_names()
     local clients = {}
-    local icon = component.icon or ' '
 
-    for _, client in pairs(vim.lsp.buf_get_clients()) do
-        clients[#clients+1] = client.name
+    for _, client in pairs(lsp.buf_get_clients(0)) do
+        clients[#clients + 1] = client.name
     end
 
-    return table.concat(clients, ' '), icon
+    return table.concat(clients, ' '), ' '
 end
 
-function M.diagnostic_errors(component)
-    local count = M.get_diagnostics_count('Error')
-    if not count or count == 0 then return '' end
-    return tostring(count), (component.icon or '  ')
+-- Common function used by the diagnostics providers
+local function diagnostics(severity)
+    local count = M.get_diagnostics_count(severity)
+
+    return count ~= 0 and tostring(count) or ''
 end
 
-function M.diagnostic_warnings(component)
-    local count = M.get_diagnostics_count('Warning')
-    if not count or count == 0 then return '' end
-    return tostring(count), (component.icon or '  ')
+function M.diagnostic_errors()
+    -- TODO: replace with diagnostic.severity.ERROR when 0.5 is no longer used
+    return diagnostics(1), '  '
 end
 
-function M.diagnostic_hints(component)
-    local count = M.get_diagnostics_count('Hint')
-    if not count or count == 0 then return '' end
-    return tostring(count), (component.icon or '  ')
+function M.diagnostic_warnings()
+    -- TODO: replace with diagnostic.severity.WARN when 0.5 is no longer used
+    return diagnostics(2), '  '
 end
 
-function M.diagnostic_info(component)
-    local count = M.get_diagnostics_count('Information')
-    if not count or count == 0 then return '' end
-    return tostring(count), (component.icon or '  ')
+function M.diagnostic_info()
+    -- TODO: replace with diagnostic.severity.INFO when 0.5 is no longer used
+    return diagnostics(3), '  '
+end
+
+function M.diagnostic_hints()
+    -- TODO: replace with diagnostic.severity.HINT when 0.5 is no longer used
+    return diagnostics(4), '  '
 end
 
 return M
+
